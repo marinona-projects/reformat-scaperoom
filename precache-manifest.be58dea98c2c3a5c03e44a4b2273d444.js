ece4ebba57ce368561aa48952c695269166886bd
marina-tests <marinagaving@gmail.com>
2020-05-20 14:19:18 +0200

    Updates

---
+++ precache-manifest.be58dea98c2c3a5c03e44a4b2273d444.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc71105cd88d02f4cc82f839d91d633",
+    "url": "/reformat-scaperoom/index.html"
+  },
+  {
+    "revision": "9a06fc4764989652b168",
+    "url": "/reformat-scaperoom/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "5c6953146f46e1024e85",
+    "url": "/reformat-scaperoom/static/css/main.ed940261.chunk.css"
+  },
+  {
+    "revision": "9a06fc4764989652b168",
+    "url": "/reformat-scaperoom/static/js/2.03df21e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reformat-scaperoom/static/js/2.03df21e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6953146f46e1024e85",
+    "url": "/reformat-scaperoom/static/js/main.c5bb7f8e.chunk.js"
+  },
+  {
+    "revision": "f724e4de2e5d52f99a20",
+    "url": "/reformat-scaperoom/static/js/runtime-main.bef737e7.js"
+  },
+  {
+    "revision": "3b7050e89c1b3730002613972d4d189f",
+    "url": "/reformat-scaperoom/static/media/boat-left.3b7050e8.png"
+  },
+  {
+    "revision": "46089fb7c5c8ed5acc91516a42d2c707",
+    "url": "/reformat-scaperoom/static/media/boat-right.46089fb7.png"
+  },
+  {
+    "revision": "a5b2358720f9b37ab43bb7e5044608a0",
+    "url": "/reformat-scaperoom/static/media/doctor.a5b23587.png"
+  },
+  {
+    "revision": "27d4e171b70d78b024b9064e0d4dadf1",
+    "url": "/reformat-scaperoom/static/media/healthy.27d4e171.png"
+  },
+  {
+    "revision": "cb6ffe3d23a7e4eadce7e95357f666db",
+    "url": "/reformat-scaperoom/static/media/kid.cb6ffe3d.png"
+  },
+  {
+    "revision": "417f8e764c4fc29922078c8f66f59571",
+    "url": "/reformat-scaperoom/static/media/sick.417f8e76.png"
+  }
+]);
